104939b6d178be6cc5784e268bd4c8c9420ee781
Ritochit Ghosh <ghosh.18.ritochit12@gmail.com>
2024-07-08 02:43:33 +0530

    Database connected, server created, best practises noted

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-// require('dotenv').config({path: './env'})
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+        console.log(`App listening on http://localhost:${process.env.PORT || 8080}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection failed @main index.js!: ", err);
+});
