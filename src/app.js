47a8f2667c54bd014cbbde1b19cbb5c47360bc5b
Ritochit Ghosh <ghosh.18.ritochit12@gmail.com>
2024-07-12 16:59:10 +0530

    utils files added, Api responses handled in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
 import express from "express";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { cookie } from "express/lib/response";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+// handling json responses from API calls
+app.use(express.json({
+    limit: "16kb"
+}));
+
+// handling file uploading config : multer
+
+//handling url requests
+app.use(express.urlencoded({
+    extended: true,
+    limit : "16kb",
+}));
+
+// to store static informations temporarily
+app.use(express.static("public"));
+
+app.use(cookieParser());
 export { app };
